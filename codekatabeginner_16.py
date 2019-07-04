cbcfeeefb9b904fe775a3df3e19ab8d21aa08810
Tourist_from_future <40977017+Madhura12gj@users.noreply.github.com>
2019-07-04 15:07:56 +0530

    Create codekatabeginner_16.py

---
+++ codekatabeginner_16.py
@@ -0,0 +1,10 @@
+
+l= int(input())  
+u= int(input())  
+for num in range(l+1,u):  
+  if num > 1:
+    for i in range(2,num):
+       if (num % i) == 0:
+         break
+    else:
+      print(num) 
