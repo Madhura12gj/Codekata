6e7d26c4c49eadb6b1dcf45f93ba0a91abbf4b9b
Tourist_from_future <40977017+Madhura12gj@users.noreply.github.com>
2019-07-04 22:36:20 +0530

    Update codekatabeginner_16.py

--- codekatabeginner_16.py
+++ codekatabeginner_16.py
@@ -1,7 +1,7 @@
-l,u =input().split()
-l= int(l)  
-u= int(u)  
-for num in range(l+1,u):  
+m,a =input().split()
+m= int(m)  
+a= int(a)  
+for num in range(m+1,a):  
   if num > 1:
     for i in range(2,num):
        if (num % i) == 0:
